6add9e1e72b9f2484a6de631c0264e950ff0544b
daniel140280 <daniel140280@gmail.com>
2025-11-21 11:35:00 +0000

    test

--- src/rungame/RunGame.java
+++ src/rungame/RunGame.java
@@ -5,5 +5,7 @@ import runsimulations.RunGameSimulations;
 public class RunGame {
     public static void main(String[] args) {
         new RunGameSimulations().runAllGameSimulations();
+
+        System.out.println("All Done");
     }
 }
